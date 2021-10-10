8072549562ad6b5db0f0258b1bca35b2f4b24353
David Lorenzo <17401854+David-Lor@users.noreply.github.com>
2021-10-10 11:50:02 +0200

    Fix socat command; log when command ends

--- forwarder/forwarder.go
+++ forwarder/forwarder.go
@@ -10,13 +10,15 @@ func forwardPort(port *PortForward) {
 	fmt.Printf("Forwarding port %s ...\n", port.ToString())
 
 	// socat TCP-LISTEN:80,fork TCP:202.54.1.5:80
-	localChunk := fmt.Sprintf("TCP-LISTEN:%d", port.LocalPort)
-	remoteChunk := fmt.Sprintf("TCP:%s:%d", port.RemoteHost, port.RemotePort)
+	localChunk := fmt.Sprintf("tcp-listen:%d,reuseaddr,fork", port.LocalPort)
+	remoteChunk := fmt.Sprintf("tcp:%s:%d", port.RemoteHost, port.RemotePort)
 
 	cmd := exec.Command("socat", localChunk, remoteChunk)
 	_, err := cmd.Output()
 	if err != nil {
 		fmt.Printf("Port forward for mapping %s failed with error: %s\n", port.ToString(), err.Error())
+	} else {
+		fmt.Printf("Port forward for mapping %s closed without error\n", port.ToString())
 	}
 }
 
